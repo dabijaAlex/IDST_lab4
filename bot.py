5a5b50f3dad345a04653dbd1d95070bc6f1ed6e5
Dabija Alex <dabijaalex27@gmail.com>
2023-12-12 19:25:21 +0200

    bot
    Signed-off-by: Dabija Alex <dabijaalex27@gmail.com>

--- bot.py
+++ bot.py
@@ -1,4 +1,26 @@
 #!./.venv/bin/python
+#Persoana mea, hereby disclaims all copyright interest in 
+#the program “bot.py”  
+#written by Dabija Alex.
+#
+#signature of Dabija Alex 12 Dec 2023
+#Dabija Alex, student
+#
+#Copyright 2023, 2023 Dabija Alex
+#
+#This program is free software: you can redistribute it and/or 
+#modify it under the terms of the GNU General Public License 
+#as published by the Free Software Foundation, either version 
+#3 of the License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public 
+#License along with this program. If not, see 
+#<https://www.gnu.org/licenses/>.
  
 import discord      # base discord module
 import code         # code.interact
